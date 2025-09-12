734f464e7fd8a18993e025f9779a72c696527d15
cancoretamero <juanjose.r@aisagroup.ca>
2025-09-12 11:59:47 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,6 @@
 // Clave pública (quedará visible si publicas el sitio)
 window.METALS_API_KEY = "0zs12hfbt7uf6jf5brv7xpq8o8175lpnwmpamvqkoz238mjqjyxxhdji4fb4";
+
+// EPITOME: URL del backend y flag para activarlo en el front
+window.EPITOME_API = window.EPITOME_API || "http://localhost:9000";
+window.EPITOME_ON = true;

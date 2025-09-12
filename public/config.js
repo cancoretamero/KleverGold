8e367f2e5902b6534ec425460d2e2a448abd5db6
cancoretamero <juanjose.r@aisagroup.ca>
2025-09-12 20:54:38 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -8,8 +8,5 @@ window.METALS_API_KEY = window.METALS_API_KEY || "0zs12hfbt7uf6jf5brv7xpq8o8175l
 window.EPITOME_API = "https://klevergold-epitome.onrender.com";
 window.EPITOME_ON = true;
 
-// (Opcional, sólo si tu código lo necesita)
-// Si no tienes una variable global CSV_URL definida en otro sitio,
-// puedes descomentar la siguiente línea como respaldo:
-//
+// (Opcional)
 // window.CSV_URL = window.CSV_URL || "./data/xauusd_ohlc_clean.csv";
